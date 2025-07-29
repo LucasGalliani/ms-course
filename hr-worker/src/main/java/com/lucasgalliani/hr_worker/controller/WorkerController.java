ce76cceec13835b474aee3efeea86ae31859f329
Lucas Galliani <lucasgalliani@uni9.edu.br>
2025-07-28 21:03:56 -0300

    Private git repository e implementação actuator

--- hr-worker/src/main/java/com/lucasgalliani/hr_worker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/lucasgalliani/hr_worker/controller/WorkerController.java
@@ -5,6 +5,8 @@ import com.lucasgalliani.hr_worker.service.WorkerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,27 +19,35 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/workers")
+@RefreshScope
 public class WorkerController {
 
     private static final Logger logger = LoggerFactory.getLogger(WorkerController.class);
+
+    @Value("${test.config}")
+    private String testConfig;
 
     @Autowired
     private WorkerService workerService;
 
     @Autowired
-    private Environment env;  // para pegar a porta atual
+    private Environment env;
+
+    @GetMapping("/configs")
+    public ResponseEntity<Void> getConfig(){
+        logger.info("CONFIG: " + testConfig);
+        return ResponseEntity.noContent().build();
+    }
+
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll(){
-        String port = env.getProperty("server.port");
-        logger.info("Requisição findAll recebida na instância da porta: {}", port);
 
         return workerService.findByAll();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Optional<Worker>> findById(@PathVariable Long id) throws InterruptedException {
-
         return workerService.findById(id);
     }
 }
